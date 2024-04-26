5c59c47e3abd69c6950159bb1e9a5809b7436500
Evan <evan.chang@gmail.com>
2024-04-25 22:36:53 -0700

    minor changes

--- autogen_poc/chatclasses/testbot.py
+++ autogen_poc/chatclasses/testbot.py
@@ -3,11 +3,8 @@ from streamlist_chat import StreamlitAppBase
 bot = StreamlitAppBase("Act as an interviewer.")
 bot.run()
 
-# Loop through the messages list and print each message.
+# print messages in terminal
 print(bot.messages[-2])
 print(bot.messages[-1])
 
-'''
-for message in bot.messages:
-    print(f'Role: {message["role"]}, Content: {message["content"]}')
-'''
+# Need to think about how we can use this class. e.g. if bot.messages % 5 == 0, send an api request from here for an updated vocab scoring list
